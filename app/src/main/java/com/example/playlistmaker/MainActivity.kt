0be5da4f61e9e17267f93146f8b61dfd3703f01b
Ecballium elaterium <amanzholaimov@gmail.com>
2025-01-22 04:57:02 +0500

    Merge remote-tracking branch 'origin/dev' into dev
    # Conflicts:
    #	app/src/main/java/com/example/playlistmaker/MainActivity.kt
    #	app/src/main/java/com/example/playlistmaker/MediaLibraryActivity.kt
    #	app/src/main/java/com/example/playlistmaker/SearchActivity.kt
    #	app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
    #	app/src/main/res/layout/activity_settings.xml
    #	app/src/main/res/layout/rounded_button.xml
    #	app/src/main/res/layout/rounded_button1.xml
    #	app/src/main/res/layout/rounded_button2.xml

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,18 +3,9 @@ package com.example.playlistmaker
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.Switch
-import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
