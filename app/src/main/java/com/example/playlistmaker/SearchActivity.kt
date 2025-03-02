8b723284ffcfbf948a51b215410543e1a671a700
Ecballium elaterium <amanzholaimov@gmail.com>
2025-03-03 01:29:31 +0500

    Sprint 11 (home work) Ооо нашел лучше решение просто проверить интернет для второй ошибки и переходы между 2мя ошибками

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -254,6 +257,12 @@ class SearchActivity : BaseActivity() {
         if (query.isNotEmpty()) {
             lastSearchQuery = query
 
+            if (!isNetworkAvailable(this)) {
+                clearTracksAndShowError(false)
+                showErrorPlaceholder(false)
+                return
+            }
+
             apiService.search(query).enqueue(object : Callback<SearchResponse> {
                 override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
                     if (response.isSuccessful && response.body() != null) {
@@ -290,5 +299,12 @@ class SearchActivity : BaseActivity() {
     
     fun getAdapter(): TrackAdapter {
         return adapter
+    }
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return networkCapabilities != null && (networkCapabilities.hasTransport(
+            NetworkCapabilities.TRANSPORT_WIFI) || networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR))
     }
 }
