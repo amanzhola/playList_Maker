ffe1f5e71efdf2af0726af060020aa2d8b764630
Ecballium elaterium <amanzholaimov@gmail.com>
2025-09-14 07:29:13 +0500

    Sprint 23(home work) Gr26
    Решен вопрос с ограничением Foreca в 30 дней и далее годовая оплата через wttr
    
    Updates done:
    1) SearchWeather - back up for Foreca[payable] via wttr.in[free] Игорь Чубаров (Igor Chubin))
    https://github.com/chubin/wttr.in
    2) location_item.xml update background to fit with night theme SearchWeather background
    3) created background color for location_item.xml background
    4) add strings (+en) for cover back up foreca with wttr
    5) SnackbarExt.kt + for Activities replaced Toast by Snackbar in SearchWeather

--- app/src/main/java/com/example/playlistmaker/utils/SnackbarExt.kt
+++ app/src/main/java/com/example/playlistmaker/utils/SnackbarExt.kt
@@ -1,9 +1,21 @@
 package com.example.playlistmaker.utils
 
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
-fun Fragment.showLongSnack(text: CharSequence, durationMs: Int = 4000) {
+// for WeatherSearch Activity to replace Toasts
+fun Activity.showLongSnack(text: CharSequence, durationMs: Int = 3000) {
+    val root = findViewById<View>(android.R.id.content)
+    val sb = Snackbar.make(root, text, Snackbar.LENGTH_LONG)
+    (sb.view.layoutParams as? ViewGroup.MarginLayoutParams)?.setMargins(0, 0, 0, 0)
+    sb.duration = durationMs
+    sb.show()
+}
+
+fun Fragment.showLongSnack(text: CharSequence, durationMs: Int = 3000) {
     val root = requireActivity().findViewById(android.R.id.content) as android.view.View
     val sb = Snackbar.make(root, text, Snackbar.LENGTH_LONG)
     (sb.view.layoutParams as? android.view.ViewGroup.MarginLayoutParams)?.setMargins(0, 0, 0, 0)
